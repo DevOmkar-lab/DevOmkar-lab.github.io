1b1b76e9d97c61a4dc79e1a386c2a29079fc3219
DevOmkar-lab <omkaragarwal00@gmail.com>
2025-10-15 23:49:37 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,7 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyBvsvQtWxr5bC5hBmLlVMEx2-booevI4C4",
-    authDomain: "auth-demo-f4fed.firebaseapp.com",
-    projectId: "auth-demo-f4fed",
-    storageBucket: "auth-demo-f4fed.firebasestorage.app",
-    messagingSenderId: "571164161011",
-    appId: "1:571164161011:web:7571f01b5ca5e7d19a1886",
-    measurementId: "G-2J82NXWWHD"
+   
   };
 
   // Initialize Firebase
@@ -25,3 +19,4 @@
 
   export const auth = getAuth(app);
   export const db = getFirestore(app);
+
