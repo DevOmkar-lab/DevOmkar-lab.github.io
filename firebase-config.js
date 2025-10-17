477c590c94b17054ef70de81e6e021c82949cd54
DevOmkar-lab <omkaragarwal00@gmail.com>
2025-10-17 14:56:32 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,27 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "",
+    authDomain: "mudrasan.firebaseapp.com",
+    projectId: "mudrasan",
+    storageBucket: "mudrasan.firebasestorage.app",
+    messagingSenderId: "563016320579",
+    appId: "1:563016320579:web:a3c065a84c03bea40576f6",
+    measurementId: "G-KD2R14NK2V"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  // Initialize other Firebase services and export them for use in pages
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
